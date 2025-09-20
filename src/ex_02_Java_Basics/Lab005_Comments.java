39ed39d7c62cbbdc063b20c44fc6ad32ce8e71ab
Pankaj <pankajjadhav55@gmail.com>
2025-09-20 13:46:09 +0530

    learning java multi Comments

---
+++ src/ex_02_Java_Basics/Lab005_Comments.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics;
+
+public class Lab005_Comments {
+    public static void main(String[] args) {
+        // This is a comment for testing
+        // This is a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab006_Comments_Multi.java
@@ -0,0 +1,33 @@
+package ex_02_Java_Basics;
+/*
+ * Author : Pankaj Jadhav
+ * Course :  Java programming
+ * Learning :  Core Java concepts
+ *
+ */
+
+public class Lab006_Comments_Multi {
+/*
+ Author : Pankaj Jadhav
+ Course :  Java programming
+ Learning :  Core Java concepts
+
+  */
+public static void main(String[] args) {
+    /*
+      Author : Pankaj Jadhav
+      Course :  Automation testing
+      Learning :  Java
+
+     */
+
+       /*
+
+        System.out.println("Hello");
+      */
+        System.out.println("Hello");
+
+}
+}
+
+
