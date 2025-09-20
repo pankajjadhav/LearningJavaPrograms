15fca12719a26cf60d20451f55f6b8ee90ed47d0
Pankaj <pankajjadhav55@gmail.com>
2025-09-21 01:06:54 +0530

    learning java Interview QnA

---
+++ src/ex_02_Java_Basics/Lab020_Interview_QnA2.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics;
+
+public class Lab020_Interview_QnA2 {
+    public static void main(String[] args) {
+//        int enum = 10;
+        int Enum = 10;
+//        System.out.println(enum);
+        //        int var  = 89;
+        int jv$var = 90;
+        int Lab020_Interview_QnA2 = 123;
+
+    }
+}
