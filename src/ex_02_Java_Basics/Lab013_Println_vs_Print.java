7cec7f40e273a97a01b298d5da5e43329b29bdb1
Pankaj <pankajjadhav55@gmail.com>
2025-09-21 00:52:23 +0530

    learning java common math exceptions

---
+++ src/ex_02_Java_Basics/Lab013_Println_vs_Print.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab013_Println_vs_Print {
+    public static void main(String[] args) {
+       /* System.out.println("Hi");
+        System.out.println("Bye");*/  // ln -> newline
+
+        System.out.print("Hello ");
+        System.out.print("Bye");
+    }
+}
