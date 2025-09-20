01242522273565581abffe5a21feff9c46db83ea
Pankaj <pankajjadhav55@gmail.com>
2025-09-21 01:16:12 +0530

    learning java data types

---
+++ src/ex_02_Java_Basics/Lab023_Byte.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class Lab023_Byte {
+    public static void main(String[] args) {
+        byte age = 10;
+
+//        String s = "Pramod";
+//        // p,r,a,m,o,d -> char - Primitive
+//
+    }
+
+}
