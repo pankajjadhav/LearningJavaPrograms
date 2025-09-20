c2be637a3d59d5b9e191fb60196e4193a8e6f90f
Pankaj <pankajjadhav55@gmail.com>
2025-09-21 01:09:28 +0530

    learning java boolean type

---
+++ src/ex_02_Java_Basics/Lab021_Boolean.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab021_Boolean {
+    public static void main(String[] args) {
+        boolean is_pankaj_married = true;
+        boolean pankaj_has_bmw = false;
+        boolean this_is_a_long_name_that_student_asked_from_us_that_is_it_possible = true;
+
+    }
+
+}
