b97ddd08275acaa2ebafddb476f49774796897b8
Pankaj <pankajjadhav55@gmail.com>
2025-09-20 19:40:40 +0530

    learning java multi Comments

---
+++ src/ex_02_Java_Basics/Lab007_Comments.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basics;
+
+public class Lab007_Comments {
+
+    /**
+     * Author - Pankaj
+     * Course :  selenium automation with java
+     * This is a documentation comment
+     */
+
+    public static void main(String[] args) {
+        // Method implementation
+
+        //        System.out.println("Hello");
+       // System.out.println("World");
+
+    }
+}
