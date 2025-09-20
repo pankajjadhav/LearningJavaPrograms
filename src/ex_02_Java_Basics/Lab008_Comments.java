87ec566cfe98c7b0cf122c1110780051cd97d1dc
Pankaj <pankajjadhav55@gmail.com>
2025-09-21 00:46:51 +0530

    learning java Keywords_identifier

---
+++ src/ex_02_Java_Basics/Lab008_Comments.java
@@ -0,0 +1,20 @@
+package ex_02_Java_Basics;
+
+public class Lab008_Comments {
+
+    /**
+     * Author - Pankaj
+     * Course :  Java programming
+     * This is a documentation comment
+     */
+
+    public static void main(String[] args) {
+        // Method implementation
+
+        //        System.out.println("Hello");
+        //        System.out.println("World!");
+
+    }
+
+
+}
