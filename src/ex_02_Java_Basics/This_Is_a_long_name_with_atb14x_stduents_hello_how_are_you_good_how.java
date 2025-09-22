9591a966d79767e6fcfa8b793617d525958d1c16
Pankaj <pankajjadhav55@gmail.com>
2025-09-22 12:19:56 +0530

    learning java - long name test

--- src/ex_02_Java_Basics/This_Is_a_long_name_with_atb14x_stduents_hello_how_are_you_good_how.java
+++ src/ex_02_Java_Basics/This_Is_a_long_name_with_atb14x_stduents_hello_how_are_you_good_how.java
@@ -1,7 +1,10 @@
 package ex_02_Java_Basics;
 
 public class This_Is_a_long_name_with_atb14x_stduents_hello_how_are_you_good_how {
-    String.out.printin("This_Is_a_long_name_with_atb14x_stduents_hello_how_are_you_good_how ");
-    // java: <identifier> expected
-    // java: illegal start of type
+    public static void main(String[] args) {
+
+        System.out.println ("This_Is_a_long_name_with_atb14x_stduents_hello");
+        // java: <identifier> expected
+        // java: illegal start of type
+    }
 }
