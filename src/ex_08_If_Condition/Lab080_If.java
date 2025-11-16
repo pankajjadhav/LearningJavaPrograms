94c1ef402efb2c09565a6dd8a6899ae3cb77e3da
pankajjadhav <pankajjadhav55@gmail.com>
2025-11-16 12:38:03 +0530

    Updating the LearningJavaPrograms

---
+++ src/ex_08_If_Condition/Lab080_If.java
@@ -0,0 +1,12 @@
+package ex_08_If_Condition;
+
+public class Lab080_If {
+    public static void main(String[] args) {
+
+        int age= 50;
+        if(age > 18){
+            System.out.println("Yes you are allowed to vote!");
+        }
+
+    }
+}
